2717a054ccc0c5c597fd56e09154d8a908097e39
Niklas May <hello@niklas-may.de>
2020-05-01 20:15:13 +0200

    try catch

--- src/functions/checkDropbox.js
+++ src/functions/checkDropbox.js
@@ -3,21 +3,48 @@ var fetch = require('isomorphic-fetch'); // or another library of choice.
 const Dropbox = require("dropbox/dist/Dropbox-sdk.min").Dropbox;
 
 
+async function listFiles(dbx, path,) {
+  return dbx.filesListFolder({ path })
+}
+
+async function getData(dbx, path) {
+  try {
+    const files = await listFiles(dbx, path)
+    return files
+  } catch (e) {
+    console.warn(e.error)
+    return []
+  }
+}
+
 export async function handler(event, context, callback) {
   var dbx = new Dropbox({ accessToken: `${process.env.DROPBOX_TOKEN}`, fetch: fetch });
 
-  dbx.filesListFolder({path: "/_Update_Page"})
-  .then(function(response) {
+  try {
+    const data = await getData(dbx, `${process.env.DROPBOX_BUILD_FOLDER}`)
+    console.log(data)
     callback(null, {
       statusCode: 200, 
-      body: JSON.stringify({response}),
+      body: JSON.stringify({data}),
     })
-  })
-  .catch(function(error) {
+  } catch(err) {
     callback(null, {
       statusCode: 400, 
-      body: JSON.stringify({error}),
+      body: JSON.stringify({err}),
     })
-  });
+  }
+  // dbx.filesListFolder({path: `${process.env.DROPBOX_BUILD_FOLDER}`})
+  // .then(function(response) {
+    // callback(null, {
+    //   statusCode: 200, 
+    //   body: JSON.stringify({response}),
+    // })
+  // })
+  // .catch(function(error) {
+  //   callback(null, {
+  //     statusCode: 400, 
+  //     body: JSON.stringify({error}),
+  //   })
+  // });
 }
 
