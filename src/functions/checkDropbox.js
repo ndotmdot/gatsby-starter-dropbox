4ab8aa0fa921897f2b1e4407b17cb8aa4cebe674
Niklas May <hello@niklas-may.de>
2020-05-02 14:28:01 +0200

    build hook var

--- src/functions/checkDropbox.js
+++ src/functions/checkDropbox.js
@@ -4,7 +4,9 @@ require('dotenv').config({ path: '.env' });
 var fetch = require('isomorphic-fetch'); // or another library of choice.
 const Dropbox = require("dropbox/dist/Dropbox-sdk.min").Dropbox;
 
-const buildHook = process.env.NODE_ENV == "development" ? process.env.MOCK_BUILD_HOOK : process.env.NETLIFY_BUILD_HOOK
+// const buildHook = process.env.NODE_ENV == "development" ? process.env.MOCK_BUILD_HOOK : process.env.NETLIFY_BUILD_HOOK
+const buildHook = process.env.NETLIFY_BUILD_HOOK
+
 console.log("buildHook", buildHook)
 
 async function listFiles(dbx, path) {
